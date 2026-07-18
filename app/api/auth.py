46213234a93b3264854960b31420f6b57a726f0f
Muhammad Umar <m.umark519@gmail.com>
2026-07-19 02:13:44 +0500

    refactor: clean authentication API routes

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,33 +1,30 @@
-import app.services.auth_service as auth_service
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.auth import (
-    hash_password,
-    verify_password,
-    create_access_token,
-    get_current_user,
-)
-from app.schemas import User
+from app import schemas
+from app import auth
+from app.services import auth_service
+
 
 router = APIRouter()
 
-users = {}
 
 @router.post("/register")
-def register(user: User):
+def register(user: schemas.User):
     return auth_service.register(user)
 
 
 @router.post("/login")
 def login(
-    form_data: OAuth2PasswordRequestForm = Depends()
+    form_data: OAuth2PasswordRequestForm = Depends(),
 ):
     return auth_service.login(form_data)
 
 
 @router.get("/profile")
-def profile(current_user: str = Depends(get_current_user)):
+def profile(
+    current_user: str = Depends(auth.get_current_user),
+):
     return {
         "message": f"Welcome {current_user}!"
     }
